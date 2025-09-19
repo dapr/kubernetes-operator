58595eb9ea4cba490a1238d6337ea7b9a0559df8
Luca Burgazzoli <lburgazzoli@users.noreply.github.com>
2025-09-19 12:45:54 +0200

    Update dependencies and generated client code (#351)
    Signed-off-by: Luca Burgazzoli <lburgazzoli@gmail.com>

--- pkg/client/applyconfiguration/operator/v1alpha1/daprcontrolplane.go
+++ pkg/client/applyconfiguration/operator/v1alpha1/daprcontrolplane.go
@@ -81,6 +81,7 @@ func extractDaprControlPlane(daprControlPlane *operatorv1alpha1.DaprControlPlane
 	b.WithAPIVersion("operator.dapr.io/v1alpha1")
 	return b, nil
 }
+func (b DaprControlPlaneApplyConfiguration) IsApplyConfiguration() {}
 
 // WithKind sets the Kind field in the declarative configuration to the given value
 // and returns the receiver, so that objects can be built by chaining "With" function invocations.
@@ -256,8 +257,24 @@ func (b *DaprControlPlaneApplyConfiguration) WithStatus(value *DaprControlPlaneS
 	return b
 }
 
+// GetKind retrieves the value of the Kind field in the declarative configuration.
+func (b *DaprControlPlaneApplyConfiguration) GetKind() *string {
+	return b.TypeMetaApplyConfiguration.Kind
+}
+
+// GetAPIVersion retrieves the value of the APIVersion field in the declarative configuration.
+func (b *DaprControlPlaneApplyConfiguration) GetAPIVersion() *string {
+	return b.TypeMetaApplyConfiguration.APIVersion
+}
+
 // GetName retrieves the value of the Name field in the declarative configuration.
 func (b *DaprControlPlaneApplyConfiguration) GetName() *string {
 	b.ensureObjectMetaApplyConfigurationExists()
 	return b.ObjectMetaApplyConfiguration.Name
+}
+
+// GetNamespace retrieves the value of the Namespace field in the declarative configuration.
+func (b *DaprControlPlaneApplyConfiguration) GetNamespace() *string {
+	b.ensureObjectMetaApplyConfigurationExists()
+	return b.ObjectMetaApplyConfiguration.Namespace
 }
